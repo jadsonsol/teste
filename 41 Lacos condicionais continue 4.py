fe78d8f17243bf7cace5d9bdead676d82be8c94c
Jadson Oliveira <jadsonsol@gmail.com>
2014-11-11 16:49:42 -0300

    First commit

---
+++ 41 Lacos condicionais continue 4.py
@@ -0,0 +1,17 @@
+#A instrução "continue" interrompe a execução de um único ciclo de um laço de repetição.
+#A instrução "break" interrompe todos os ciclos de um laço de repetição.
+print("Antes")
+print()
+print("inicio")
+i = 0
+while(i<10):
+    i += 1
+    if(i%2==0):
+        continue
+    if(i>5):
+        break
+    print(i)
+else:
+    print("else")
+print("fim")
+print()
